9acc93419582dc87e55f5631a07eceae668acd7e
bluelovers <codelovers@users.sourceforge.net>
2019-06-08 12:46:20 +0800

    publish new version

--- packages/ts-type/lib/type/base.d.ts
+++ packages/ts-type/lib/type/base.d.ts
@@ -3,3 +3,4 @@
  */
 export declare type ITSArrayListMaybeReadonly<T> = T[] | readonly T[];
 export declare type ITSKeys = symbol | string | number;
+export declare type ITSConstructorLike<T extends any = any> = new (...args: any) => T;
--- packages/ts-type/lib/type/base.ts
+++ packages/ts-type/lib/type/base.ts
@@ -5,3 +5,5 @@
 export type ITSArrayListMaybeReadonly<T> = T[] | readonly T[];
 
 export type ITSKeys = symbol | string | number;
+
+export type ITSConstructorLike<T extends any = any> = new(...args: any) => T;
