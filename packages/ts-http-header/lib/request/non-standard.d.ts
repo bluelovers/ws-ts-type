380c749905c95f2a7af522ed3c6b86c704e2da49
bluelovers <codelovers@users.sourceforge.net>
2019-06-11 16:07:30 +0800

    Partial

--- packages/ts-http-header/lib/request/non-standard.d.ts
+++ packages/ts-http-header/lib/request/non-standard.d.ts
@@ -1,19 +1,19 @@
 export interface ICommonNonStandardRequestFields {
-    'Upgrade-Insecure-Requests': string;
-    'X-Requested-With': string;
-    'DNT': string;
-    'X-Forwarded-For': string;
-    'X-Forwarded-Host': string;
-    'X-Forwarded-Proto': string;
-    'Front-End-Https': string;
-    'X-Http-Method-Override': string;
-    'X-ATT-DeviceId': string;
-    'X-Wap-Profile': string;
-    'Proxy-Connection': string;
-    'X-UIDH': string;
-    'X-Csrf-Token': string;
-    'X-Request-ID': string;
-    'X-Correlation-ID': string;
-    'Save-Data': string;
+    'Upgrade-Insecure-Requests'?: string;
+    'X-Requested-With'?: string;
+    'DNT'?: string;
+    'X-Forwarded-For'?: string;
+    'X-Forwarded-Host'?: string;
+    'X-Forwarded-Proto'?: string;
+    'Front-End-Https'?: string;
+    'X-Http-Method-Override'?: string;
+    'X-ATT-DeviceId'?: string;
+    'X-Wap-Profile'?: string;
+    'Proxy-Connection'?: string;
+    'X-UIDH'?: string;
+    'X-Csrf-Token'?: string;
+    'X-Request-ID'?: string;
+    'X-Correlation-ID'?: string;
+    'Save-Data'?: string;
 }
 export default ICommonNonStandardRequestFields;
