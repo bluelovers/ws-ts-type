f588bd1642340f9b1846801a0b04db863cfa5d98
bluelovers <codelovers@users.sourceforge.net>
2021-07-16 14:12:37 +0800

    build: update js

--- packages/package-dts/index.js
+++ packages/package-dts/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.readPackageJson = void 0;
 const fs_1 = require("fs");
 function readPackageJson(file) {
-    return JSON.parse(fs_1.readFileSync(file).toString());
+    return JSON.parse((0, fs_1.readFileSync)(file).toString());
 }
 exports.readPackageJson = readPackageJson;
 //# sourceMappingURL=index.js.map
