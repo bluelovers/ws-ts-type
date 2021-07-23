aadd6daee07765950ac2e19712c9b68a326c1e82
bluelovers <codelovers@users.sourceforge.net>
2021-07-23 19:26:05 +0800

    .

--- packages/ts-global-type-extra/index.d.ts
+++ packages/ts-global-type-extra/index.d.ts
@@ -6,9 +6,4 @@ declare global
 		entries<V, K extends string>(o: { [key in K]: V } | ArrayLike<V> | readonly V[]): [K, V][];
 	}
 
-	interface ArrayConstructor
-	{
-		isArray(arg: any): arg is readonly any[] | any[];
-	}
-
 }
