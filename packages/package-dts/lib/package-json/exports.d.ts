8ea01fd2c12b7cb0d853532b4e50af937128613c
bluelovers <codelovers@users.sourceforge.net>
2022-10-11 01:08:41 +0800

    .

--- packages/package-dts/lib/package-json/exports.d.ts
+++ packages/package-dts/lib/package-json/exports.d.ts
@@ -1,35 +1,37 @@
-import { PackageExportsEntry, PackageExportsEntryObject, PackageExportsFallback, JSONSchemaForNPMPackageJsonFiles2 as CoreProperties } from '../../types/package.json';
+import { PackageExportsEntryObject, PackageExportsFallback, PackageExportsEntryPath } from '../../types/package.json';
 import { ITSPartialRecord } from 'ts-type/lib/type/record/partial';
 type _IConditions = 'node-addons' | 'node' | 'default' | 'types' | 'import' | 'require';
-interface _IPackageExportsEntryObjectExtend extends ITSPartialRecord<_IConditions, PackageExportsEntry | PackageExportsFallback> {
+interface _IPackageExportsEntryObjectExtend extends ITSPartialRecord<_IConditions, IPackageExportsValue> {
 }
 declare module '../../types/package.json' {
     interface PackageExportsEntryObject extends _IPackageExportsEntryObjectExtend {
-        [k: string]: PackageExportsEntry | PackageExportsFallback;
+        [k: string]: IPackageExportsValue;
     }
 }
 export interface IPackageExportsEntryObject extends PackageExportsEntryObject, _IPackageExportsEntryObjectExtend {
 }
+export type IPackageExportsValueFallback = PackageExportsEntryPath | PackageExportsFallback;
+export type IPackageExportsValue = IPackageExportsValueFallback | IPackageExportsEntryObject;
 /**
  * The module path prefix that is resolved when the module specifier starts with "name/", set to "./*" to allow external modules to import any subpath.
  *
  * This interface was referenced by `undefined`'s JSON-Schema definition
  * via the `patternProperty` "^\./.+".
  */
-export interface IPackageJsonExportsEntryObjectRoot extends ITSPartialRecord<'default' | './package.json' | `./${string}` | _IConditions, PackageExportsEntry | PackageExportsFallback | IPackageExportsEntryObject> {
+export interface IPackageJsonExportsEntryObjectRoot extends ITSPartialRecord<'default' | './package.json' | `./${string}` | _IConditions, IPackageExportsValue> {
     /**
      * The module path that is resolved when the module specifier matches "name", shadows the "main" field.
      */
-    "."?: PackageExportsEntry | PackageExportsFallback | IPackageExportsEntryObject;
+    "."?: IPackageExportsValue;
     /**
      * The module path prefix that is resolved when the module specifier starts with "name/", set to "./*" to allow external modules to import any subpath.
      *
      * This interface was referenced by `undefined`'s JSON-Schema definition
      * via the `patternProperty` "^\./.+".
      */
-    [k: string]: PackageExportsEntry | PackageExportsFallback | IPackageExportsEntryObject;
+    [k: string]: IPackageExportsValue;
 }
 export interface IPackageJsonExports {
-    exports?: IPackageJsonExportsEntryObjectRoot | CoreProperties["exports"];
+    exports?: IPackageExportsValueFallback | IPackageJsonExportsEntryObjectRoot;
 }
 export {};
--- packages/package-dts/lib/package-json/exports.ts
+++ packages/package-dts/lib/package-json/exports.ts
@@ -1,10 +1,12 @@
-import { PackageExportsEntry, PackageExportsEntryObject, PackageExportsFallback,
-	JSONSchemaForNPMPackageJsonFiles2 as CoreProperties } from '../../types/package.json';
+import {
+	PackageExportsEntry, PackageExportsEntryObject, PackageExportsFallback,
+	JSONSchemaForNPMPackageJsonFiles2 as CoreProperties, PackageExportsEntryPath,
+} from '../../types/package.json';
 import { ITSPartialRecord } from 'ts-type/lib/type/record/partial';
 
 type _IConditions = 'node-addons' | 'node' | 'default' | 'types' | 'import' | 'require';
 
-interface _IPackageExportsEntryObjectExtend extends ITSPartialRecord<_IConditions, PackageExportsEntry | PackageExportsFallback>
+interface _IPackageExportsEntryObjectExtend extends ITSPartialRecord<_IConditions, IPackageExportsValue>
 {
 
 }
@@ -13,7 +15,7 @@ declare module '../../types/package.json'
 {
 	interface PackageExportsEntryObject extends _IPackageExportsEntryObjectExtend
 	{
-		[k: string]: PackageExportsEntry | PackageExportsFallback;
+		[k: string]: IPackageExportsValue;
 	}
 }
 
@@ -22,18 +24,22 @@ export interface IPackageExportsEntryObject extends PackageExportsEntryObject, _
 
 }
 
+export type IPackageExportsValueFallback = PackageExportsEntryPath | PackageExportsFallback;
+
+export type IPackageExportsValue = IPackageExportsValueFallback | IPackageExportsEntryObject;
+
 /**
  * The module path prefix that is resolved when the module specifier starts with "name/", set to "./*" to allow external modules to import any subpath.
  *
  * This interface was referenced by `undefined`'s JSON-Schema definition
  * via the `patternProperty` "^\./.+".
  */
-export interface IPackageJsonExportsEntryObjectRoot extends ITSPartialRecord<'default' | './package.json' | `./${string}` | _IConditions, PackageExportsEntry | PackageExportsFallback | IPackageExportsEntryObject>
+export interface IPackageJsonExportsEntryObjectRoot extends ITSPartialRecord<'default' | './package.json' | `./${string}` | _IConditions, IPackageExportsValue>
 {
 	/**
 	 * The module path that is resolved when the module specifier matches "name", shadows the "main" field.
 	 */
-	"."?: PackageExportsEntry | PackageExportsFallback | IPackageExportsEntryObject;
+	"."?: IPackageExportsValue;
 
 	/**
 	 * The module path prefix that is resolved when the module specifier starts with "name/", set to "./*" to allow external modules to import any subpath.
@@ -41,10 +47,10 @@ export interface IPackageJsonExportsEntryObjectRoot extends ITSPartialRecord<'de
 	 * This interface was referenced by `undefined`'s JSON-Schema definition
 	 * via the `patternProperty` "^\./.+".
 	 */
-	[k: string]: PackageExportsEntry | PackageExportsFallback | IPackageExportsEntryObject;
+	[k: string]: IPackageExportsValue;
 }
 
 export interface IPackageJsonExports
 {
-	exports?: IPackageJsonExportsEntryObjectRoot | CoreProperties["exports"]
+	exports?: IPackageExportsValueFallback | IPackageJsonExportsEntryObjectRoot
 }
